9a247bc3d8fc8a32253cfbae7a3dcfe16c4816a5
kevinoff <kevinofficer@hotmail.co.uk>
2019-09-17 16:10:26 +0100

    return live web page instead of a string of text.

--- 04-02-ToyStory-RestController/src/main/java/uk/ac/belfastmet/toystory/controller/HomeController.java
+++ 04-02-ToyStory-RestController/src/main/java/uk/ac/belfastmet/toystory/controller/HomeController.java
@@ -1,16 +1,16 @@
 package uk.ac.belfastmet.toystory.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping()
 
 public class HomeController {
 
 	@GetMapping("")
 	public String home() {
-		return "Home Page";
+		return "index";
 	}
 }
