848cf7d239c53c6a246bf73e1730a8bf1e6c0f02
kevinoff <kevinofficer@hotmail.co.uk>
2019-09-18 11:56:33 +0100

    using java to popular page title

--- 04-03-dwarf-website/src/main/java/uk/ac/belfastmet/dwarfs/controller/DwarfsController.java
+++ 04-03-dwarf-website/src/main/java/uk/ac/belfastmet/dwarfs/controller/DwarfsController.java
@@ -1,8 +1,11 @@
 package uk.ac.belfastmet.dwarfs.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import uk.ac.belfastmet.dwarfs.service.DwarfService;
 
 @Controller
 @RequestMapping
@@ -10,15 +13,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class DwarfsController {
 
 	@GetMapping("")
-	public String homePage() {
+	public String homePage(Model model) {
+		model.addAttribute("pageTitle", "Home");
 		return "index";
 	}
 	@GetMapping("/disney")
-	public String disneyPage() {
+	public String disneyPage(Model model) {
+		
+		//DwarfService dwarfService = new DwarfService();
+		model.addAttribute("pageTitle", "Disney Dwarfs");
+		model.addAttribute("numberOfDwarfs", "7");
+		
 		return "disney";
 	}
 	@GetMapping("/tolkien")
-	public String tolkienPage() {
+	public String tolkienPage(Model model) {
+		model.addAttribute("pageTitle", "Tolkien Dwarfs");
 		return "tolkien";
 	}
 }
