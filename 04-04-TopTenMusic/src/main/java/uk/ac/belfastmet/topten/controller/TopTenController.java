91c2aa69e80296fe9d6c110a1eac9f3cf131ba47
kevinoff <kevinofficer@hotmail.co.uk>
2019-09-20 10:05:02 +0100

    moved from GetMapping to Request Mapping

--- 04-04-TopTenMusic/src/main/java/uk/ac/belfastmet/topten/controller/TopTenController.java
+++ 04-04-TopTenMusic/src/main/java/uk/ac/belfastmet/topten/controller/TopTenController.java
@@ -2,25 +2,25 @@ package uk.ac.belfastmet.topten.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import uk.ac.belfastmet.topten.service.TopTenService;
 
 @Controller
-@RequestMapping
+@RequestMapping()
 
 public class TopTenController {
 
 	//get home page
-	@GetMapping("")
+	@RequestMapping(value = "", method = RequestMethod.GET)
 	public String homePage(Model model) {
 		model.addAttribute("pageTitle", "Home");
 				return "index";
 	}
 	
 	//get albums page
-	@GetMapping("/albums")
+	@RequestMapping(value = "/albums", method = RequestMethod.GET)
 	public String albumsPage(Model model) {
 		model.addAttribute("pageTitle", "Albums");
 		TopTenService topTenService = new TopTenService();
@@ -29,12 +29,11 @@ public class TopTenController {
 	}
 	
 	//get singles page
-	@GetMapping("/singles")
+	@RequestMapping(value = "/singles", method = RequestMethod.GET)
 	public String singlesPage(Model model) {
 		model.addAttribute("pageTitle", "Singles");
 		TopTenService topTenService = new TopTenService();
 		model.addAttribute("topTenSingles", topTenService.getTopTenSingles());
 		return "singles";
 	}
-	
 }
